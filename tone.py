1067f1a9392f323507f8e15026d4b7a324e0dacb
NafiAtero <magfarnafi@gmail.com>
2024-01-14 23:59:54 +0700

    all pass filter

--- tone.py
+++ tone.py
@@ -33,12 +33,22 @@ def saw_wave(amp, freq, phase):
 def noise(amp):
     return random.randrange(-1, 1) * amp
 
+def all_pass_filter(input_sample, delayed_sample, target_freq):
+    tan = np.tan(np.pi * target_freq / sample_rate)
+    a1 = (tan - 1) / (tan + 1)
+    output_sample = a1 * input_sample + delayed_sample
+    delayed_sample = input_sample - a1 * output_sample
+    return output_sample, delayed_sample
+    
+
 def generate_tone(note, duration, a=0.005, d=0.005, s=0.8, r=0.010, wave="sine"):
     sample_count = int(round((duration + r) * sample_rate))
     samples = np.zeros(sample_count, dtype=np.float32)
     amp = 0
     freq = get_note_freq(note)
-    clipping = False
+    clipping = 0
+    hpf_sample = 0
+    lpf_sample = 0
     
     for i in range(sample_count):
         t = i / sample_rate
@@ -64,15 +74,22 @@ def generate_tone(note, duration, a=0.005, d=0.005, s=0.8, r=0.010, wave="sine")
         elif wave == "noise":
             sample = noise(amp)
             
+        lpf_output, lpf_sample = all_pass_filter(sample, lpf_sample, freq*2)
+        hpf_output, hpf_sample = all_pass_filter(sample, hpf_sample, freq*0.5)
+        
+        sample += lpf_output
+        sample -= hpf_output
+        sample /= 2
+            
         if sample > 1: 
-            clipping == True
+            clipping = max(clipping, sample - 1)
             sample = 1
         elif sample < -1: 
-            clipping == True
+            clipping = max(clipping, -(1 + sample))
             sample = -1
         
         samples[i] = sample
     
-    if clipping: print("clipping")
+    if clipping != 0: print("!!CLIPPING!!", clipping)
     return samples
 
