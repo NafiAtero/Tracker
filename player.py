c087215a864a3167cbbf1e23ae9ddfc8e65accf3
NafiAtero <magfarnafi@gmail.com>
2024-01-14 20:04:09 +0700

    tri & saw waves

--- player.py
+++ player.py
@@ -24,10 +24,10 @@ def play_samples(samples, volume=0.2, sample_rate=44100):
     p.terminate()
 
 print("generating tones")
-a = generate_tone(69, 2, a=0.1, d=0.2, r=0.5, wave="square")
-b = generate_tone(71, 1, a=0.1, d=0.2, r=0.5)
-c = generate_tone(72, 1, a=0.1, d=0.2, r=0.5)
-d = generate_tone(74, 1, a=0.1, d=0.2, r=0.5)
+a = generate_tone(69, 1, a=0.1, d=0.2, r=0.1, wave="sine")
+b = generate_tone(69, 1, a=0.1, d=0.2, r=0.1, wave="square")
+c = generate_tone(69, 1, a=0.1, d=0.2, r=0.1, wave="tri")
+d = generate_tone(69, 1, a=0.1, d=0.2, r=0.1, wave="saw")
 e = generate_tone(76, 2, a=0.1, d=0.2, r=0.5)
 print("tones generated")
 
@@ -37,4 +37,7 @@ print("tracks combined")
 
 print("playing")
 play_samples(a)
+play_samples(b)
+play_samples(c)
+play_samples(d)
 print("finished playing")
--- tone.py
+++ tone.py
@@ -1,20 +1,37 @@
 import numpy as np
 import math
+import random
 
 sample_rate = 44100
 
 def get_note_freq(n, base_freq=440, base_note=69):
-    if n > 108 or n < 0: return base_freq
+    if n > 108 or n < 12: return base_freq
     return base_freq * (2 ** ((n - base_note) / 12))
 
 def sine_wave(amp, freq, phase):
     return amp * math.sin(2 * math.pi * freq * phase)
 
 def square_wave(amp, freq, phase):
-    sine = sine_wave(amp, freq, phase)
+    sample = sine_wave(amp, freq, phase)
     for i in range(3, 50, 2):
-        sine += sine_wave(amp/i, freq*i, phase)
-    return sine
+        sample += sine_wave(amp/i, freq*i, phase)
+    return sample
+
+def tri_wave(amp, freq, phase):
+    sample = sine_wave(amp, freq, phase)
+    for i in range(3, 50, 4):
+        sample -= sine_wave(amp/(i**2), freq*i, phase)
+        sample += sine_wave(amp/((i+2)**2), freq*(i+2), phase)
+    return sample
+
+def saw_wave(amp, freq, phase):
+    sample = sine_wave(amp, freq, phase)
+    for i in range(2, 50):
+        sample += sine_wave(amp/i, freq*i, phase)
+    return sample
+
+def noise(amp):
+    return random.randrange(-1, 1) * amp
 
 def generate_tone(note, duration, a=0.005, d=0.005, s=0.8, r=0.010, wave="sine"):
     sample_count = int(round((duration + r) * sample_rate))
@@ -34,11 +51,18 @@ def generate_tone(note, duration, a=0.005, d=0.005, s=0.8, r=0.010, wave="sine")
             amp = (((duration + r) - t) / r) * s
         else:               # sustain
             amp = s
-            
+           
+        sample = 0
         if wave == "sine":
             sample = sine_wave(amp, freq, t)
         elif wave == "square":
             sample = square_wave(amp, freq, t)
+        elif wave == "tri":
+            sample = tri_wave(amp, freq, t)
+        elif wave == "saw":
+            sample = saw_wave(amp, freq, t)
+        elif wave == "noise":
+            sample = noise(amp)
             
         if sample > 1: 
             clipping == True
